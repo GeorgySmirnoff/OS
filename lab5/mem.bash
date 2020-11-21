d35dc23f47353ff15fd0b23355a327171ba2d795
Georgy <smirnovgeorgii78@gmail.com>
2020-11-21 05:17:29 +0300

    lab5

---
+++ lab5/mem.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+arr=() #пустой массив
+count=0  #счетчик шагов
+while :
+do
+echo "Бесконечный цикл bash, для выхода нажмите Ctrl+C"
+arr+=(1 2 3 4 5 6 7 8 9 10)
+let count++
+if [[ $count == 100000 ]]
+then
+count=0
+echo ${#array[@]} >> report.log
+fi
+done
